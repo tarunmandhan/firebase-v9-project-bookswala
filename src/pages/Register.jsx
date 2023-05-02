3972c2a96b3bc908413211be9b3b31f2e7128a45
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-03 01:48:38 +0530

    state management using useState & onChange event

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import { React, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useFirebase } from "../context/Firebase";
 
 const RegisterPage = () => {
+  const firebase = useFirebase();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  console.log(firebase);
+
   return (
     <>
       <div className="container mt-5">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="Enter email"
+            />
             <Form.Text className="text-muted">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
-          <Form.Group clbel>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="Password"
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
-            Submit
+            Create Account{" "}
           </Button>
         </Form>
       </div>
