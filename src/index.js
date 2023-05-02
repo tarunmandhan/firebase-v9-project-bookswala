b55f197204a1b5b2ab54bb907e7ef11f602df7bb
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-03 00:23:21 +0530

    again another wrap with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { FirebaseProvider } from "./context/Firebase";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FirebaseProvider>
-      <App />
-    </FirebaseProvider>
+    <BrowserRouter>
+      <FirebaseProvider>
+        <App />
+      </FirebaseProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
