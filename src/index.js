dfedb999b37a31918576f5faee84f51bd728a32b
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-02 23:51:54 +0530

    App comp. wrap with FirebaseProvider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { FirebaseProvider } from "./context/Firebase";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
