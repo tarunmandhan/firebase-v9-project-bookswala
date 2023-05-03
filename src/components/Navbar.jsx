55b1a29df00fc753add852a5ae4029ee5257450b
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-04 02:05:17 +0530

    Navbar init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const MyNavbar = () => {
+  return (
+    <>
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#home">Add Books</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+      ;
+    </>
+  );
+};
+
+export default MyNavbar;
