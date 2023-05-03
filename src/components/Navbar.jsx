14ba7343934337b4f54b3f0a98a1dc4f7447e050
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-04 02:21:47 +0530

    link pages ( / & list)  to navbar menu text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,8 @@ const MyNavbar = () => {
           <Container>
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#home">Add Books</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="list">Add Books</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
