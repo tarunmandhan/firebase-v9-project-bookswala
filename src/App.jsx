59c1d6434c451bb5d7b4eb06c85a750e71086744
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-04 01:27:55 +0530

    Add MyNavbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
+//Components
+import MyNavbar from "./components/Navbar";
+
 //pages
 import RegisterPage from "./pages/Register";
 import LoginPage from "./pages/Login";
@@ -12,6 +15,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => {
   return (
     <>
+      <MyNavbar />
       <Routes>
         <Route path="/" element={<h1>Home page</h1>} />
         <Route path="/login" element={<LoginPage />} />
