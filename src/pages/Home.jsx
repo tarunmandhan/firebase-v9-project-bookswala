19a28e741ab6d182d02097c6ecccc0f616637ca0
Tarun Mandhan <mandhantarun@gmail.com>
2023-05-05 01:12:04 +0530

    show all books list on home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useFirebase } from "../context/Firebase";
 
 const HomePage = () => {
+  const firebase = useFirebase();
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    firebase.listAllBooks().then((books) => setBooks(books.docs));
+  }, [firebase]);
+
   return (
     <>
-      <div className="container">List Books Here</div>;
+      <div className="container">
+        {books.map((book) => (
+          <li>{book.data().name}</li>
+        ))}
+      </div>
     </>
   );
 };
